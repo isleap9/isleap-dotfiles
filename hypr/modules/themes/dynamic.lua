0f418d97ae7bf15d03d6b2a885758800716f978c
isleap9 <pierluigisassanelli98@gmail.com>
2026-05-17 07:53:34 +0200

    update configs

--- hypr/modules/themes/dynamic.lua
+++ hypr/modules/themes/dynamic.lua
@@ -1,6 +1,6 @@
 hl.config({
     general = {
         ["col.active_border"]   = "rgb(ffb2ba)",
-        ["col.inactive_border"] = "rgb(1c1012)",
+        ["col.inactive_border"] = "rgb(1a1112)",
     },
 })
--- rofi/launcher.sh
+++ rofi/launcher.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-WALLPAPER=$(awww query | awk -F"'" "" | head -1)
-mkdir -p ~/.config/rofi/images
+
+WALLPAPER=$(awww query | grep -o 'image: .*' | head -1)
+WALLPAPER=${WALLPAPER#image: }
+
 ln -sf "$WALLPAPER" ~/.config/rofi/images/a.png
+
 LAYOUT_FILE="$HOME/.config/rofi/current-layout"
 LAYOUT=$(cat "$LAYOUT_FILE" 2>/dev/null || echo "style-2")
+
 rofi -show drun -hover-select -me-select-entry "" -me-accept-entry "MousePrimary" -theme "$HOME/.config/rofi/${LAYOUT}.rasi"
